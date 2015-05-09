8e5ecd0665fc44555579b898e0963a45f8698c98
Sarah Kwak <sarahkwak@hotmail.com>
2015-05-08 20:32:14 -0700

    dotenv implemented and .env is created

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,5 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :linkedin
+  config.omniauth :linkedin, ENV["LINKED_IN_API"], ENV["LINKEDIN_CONSUMER_SKEY"]
 end
