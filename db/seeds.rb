ef7eb6e7ee8aa1cb2b7f7a362518565f9c673604
Sarah Kwak <sarahkwak@hotmail.com>
2015-05-10 14:03:32 -0700

    seed file fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Question.create(title: "hello")
-# require 'JSON'
+require 'JSON'
 
-# json_data = File.read('ruby.json')
-# p json_data
-# parsed_data = JSON.parse(json_data)
-# p "*" * 100
-# p parsed_data['ruby']
-# parsed_data['ruby']
-# i = 0
-# while i< parsed_data['ruby'].length
-#   curriculum = Curriculum.create!(url: parsed_data['ruby'][i]['url'], description: parsed_data['ruby'][i]['description'], title:parsed_data['ruby'][i]['title'], curriculum_type: "Ruby")
-#   i+=1
-# end
+json_data = File.read('ruby.json')
+parsed_data = JSON.parse(json_data)
+parsed_data['ruby']
+i = 0
+while i< parsed_data['ruby'].length
+  curriculum = Curriculum.new
+  curriculum.url = parsed_data['ruby'][i]['url']
+  curriculum.description = parsed_data['ruby'][i]['description']
+  curriculum.title = parsed_data['ruby'][i]['title']
+  curriculum.curriculum_type = "Ruby"
+  curriculum.save
+  i+=1
+end
 
 # Curriculum.create!(curriculum_type: "Ryby", title: "CodeAcademy, Ruby", url: "http://www.codecademy.com/en/tracks/ruby", description: "Learn to program in Ruby, a ﬂexible language used to create sites like Codecademy")
 
