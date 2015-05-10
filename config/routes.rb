d54d74150da4d3886af5e10079fe9076224dfe88
miniyakkos <ayakokurushima@gmail.com>
2015-05-09 18:32:36 -0700

    Start work on questions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'users#index'
    get 'users/signature' => 'users#signature'
+   get '/questions' => 'questions#index'
+   post '/questions' => 'questions#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
