ac5936f078dbe64b3eb841a32707fe1a6b53255a
eshuster <eshuster3@gmail.com>
2015-05-13 00:21:54 -0700

    Add fixed nav bar for home page

--- app/assets/javascripts/button.js
+++ app/assets/javascripts/button.js
@@ -4,9 +4,10 @@ $(document).ready(function(){
     event.preventDefault();
     console.log(this)
     $(this).animate(
-      {$(this).css("background": "blue"}, 1000);
+      {background: "blue"}, 1000);
         }).mouseout(function() {
           $(this).animate(
-            {backgroundColor: "rgba(217, 101, 76, .2)" }, 500);
+            {background
+              ;, "rgba(217, 101, 76, .2)" }, 500);
           })
         });
