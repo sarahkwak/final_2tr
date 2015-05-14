c9d0b52016b2310db7f6b72150900d609df18d20
mikeylude <michaelwailim@gmail.com>
2015-05-13 22:05:11 -0700

    fix video

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.start_video').on('click', function(event) {
     event.preventDefault();
-    debugger
     CineIOPeer.init('9cd3947bc9fba6aa761c4edf5b795dc6');
     CineIOPeer.startCameraAndMicrophone();
     CineIOPeer.on('media-added', function(data){
