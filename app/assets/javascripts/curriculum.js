74416f8ada94e8e8467a329030ec8358a3b464f6
eshuster <eshuster3@gmail.com>
2015-05-14 10:02:45 -0700

    Add ajax to the top of the modal

--- app/assets/javascripts/curriculum.js
+++ app/assets/javascripts/curriculum.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
+  var list = $('<ul class="modal_bar"></ul>');
+  list.append('<li class="lst" id="html_tag">HTML/CSS</li>&nbsp');
+  list.append('<li class="lst" id="javascript_tag">Javascript</li>&nbsp');
+  list.append('<li class="lst" id="angular_tag">Angular</li>&nbsp');
+  list.append('<li class="lst" id="node_tag">Node</li>&nbsp');
+  list.append('<li class="lst" id="python">Python</li>&nbsp');
+  list.append('<li class="lst" id="mongo">Mongo</li>&nbsp');
+  list.append('<li class="lst" id="git_tag">Git</li>&nbsp');
+  list.append('<li class="lst" id="ruby_tag">Ruby</li>&nbsp');
+  list.append('<li class="lst" id="jquery_tag">Jquery</li>&nbsp');
+  list.append('<li class="lst" id="sql_tag">SQL</li>&nbsp');
+  list.append('<li class="lst" id="sublime_tag">Sublime</li>&nbsp');
 
   $('#html').on('click', function(event){
     event.preventDefault();
 
     var ul = $('<ul></ul>');
-    // var response_html = $.ajax({
-    //   url:"https://www.kimonolabs.com/api/a5ezu2ju?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
-    //   crossDomain: true,
-    //   dataType: "jsonp",
-    //   success: function (response) {
-    //     var place = 0
-    //     while (place < 6){
-    //       var linkListing = $('<li class="linkListing"></li>')
-    //       var text = response.results['collection2'][place]['Lesson-Links'];
-    //       linkListing.append(text);
-    //       ul.append(linkListing);
-    //       place++;
-    //     };
-    //   },
-    //   error: function (xhr, status) {
-    //   }
-    // });
-
     var response_CSS1 = $.ajax({
       url:"https://www.kimonolabs.com/api/9cn5msz6?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
       crossDomain: true,
@@ -55,6 +49,7 @@ $(document).ready(function(){
           ul.append(linkListing);
           place++;
         };
+        ul.prepend(list);
         var myModal = new Modal({
           content: "" + $(ul).html(),
           maxWidth: 600,
@@ -120,7 +115,9 @@ $(document).ready(function(){
             linkListing.append(a);
             ul.append(linkListing);
             place++;
-        }
+        };
+        ul.prepend(list);
+
           var myModal = new Modal({
             content: "" + $(ul).html(),
             maxWidth: 600,
@@ -168,6 +165,7 @@ $('#angular').on('click', function(event){
             ul.append(linkListing);
             place++;
           };
+        ul.prepend(list);
             var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -198,6 +196,8 @@ $('#angular').on('click', function(event){
             ul.append(linkListing);
             place++;
           };
+                  ul.prepend(list);
+
           var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -229,6 +229,7 @@ $('#angular').on('click', function(event){
 
             place++;
           };
+          ul.prepend(list);
           var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -252,6 +253,8 @@ $('#angular').on('click', function(event){
             ul.append(a);
             place++;
           };
+                  ul.prepend(list);
+
           var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -265,9 +268,9 @@ $('#angular').on('click', function(event){
 
 
 $('#python').on('click', function(event){
-
-var ul =$('<ul></ul>')
- var response_python = $.ajax({
+  event.preventDefault();
+  var ul =$('<ul></ul>')
+  var response_python = $.ajax({
     url:"https://www.kimonolabs.com/api/5z3f5btc?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
     crossDomain: true,
     dataType: "jsonp",
@@ -282,6 +285,8 @@ var ul =$('<ul></ul>')
             ul.append(linkListing);
             place++;
           };
+                  ul.prepend(list);
+
           var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -295,6 +300,7 @@ var ul =$('<ul></ul>')
   });
 
 $('#mongo').on('click', function(event){
+  event.prepeventDefault();
   var ul =$('<ul></ul>')
   var response_python = $.ajax({
     url:"https://www.kimonolabs.com/api/a0oyohre?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
@@ -310,6 +316,8 @@ $('#mongo').on('click', function(event){
             ul.append(linkListing);
             place++;
           };
+                  ul.prepend(list);
+
           var myModal = new Modal({
               content: "" + $(ul).html(),
               maxWidth: 600,
@@ -322,9 +330,293 @@ $('#mongo').on('click', function(event){
     });
   });
 
-
 $("body").on("click", "a.link", function(event){
     event.preventDefault();
     window.open(this, "_blank")
   });
+
+// $('.curriculum').on('click', function(e){
+//   $('.main-container-2').animate({'marginLeft': '+=130px'}, 'slow');
+// })
+
+// $("body").on("click", "button.close-button", function(event){
+//    $('.main-container-2').animate({'marginLeft': '-=130px'}, 'slow');
+//  });
+
+
+$("body").on("click", "li#node_tag", function(event){
+    var ul = $('<ul></ul>')
+    var response_SN1 = $.ajax({
+        url:"https://www.kimonolabs.com/api/corprlpy?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          var place = 0
+          var resources = $('<p></p>')
+          while (place < 19){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results['collection1'][place]['Link-Titles-1'].text
+            var a = $('<a class="link" href=' + response.results['collection1'][place]['Link-Titles-1'].href + '>'  + text + '</a><br>')
+            linkListing.append(a);
+            ul.append(linkListing);
+
+            place++;
+          };
+          ul.prepend(list);
+        $('.scotch-content').html(ul);
+      },
+      error: function (xhr, status) {
+      }
+    });
+  });
+
+$("body").on("click", "li#html_tag", function(event){
+  event.preventDefault();
+
+    var ul = $('<ul></ul>');
+    var response_CSS1 = $.ajax({
+      url:"https://www.kimonolabs.com/api/9cn5msz6?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+      crossDomain: true,
+      dataType: "jsonp",
+      success: function (response) {
+        var place = 0
+        while (place < 19 ){
+          var linkListing = $('<li class="linkListing"></li>')
+          var text = response.results.collection1[place]["Link-Titles"].text;
+          var a = $('<a class="link" href=' + response.results.collection1[place]["Link-Titles"].href + '>'  + text + '</a><br>')
+          linkListing.append(a);
+          ul.append(linkListing);
+          place++;
+        };
+      },
+      error: function (xhr, status) {
+      }
+    });
+
+    var response_CSS2 = $.ajax({
+      url:"https://www.kimonolabs.com/api/6wrt9py8?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+      crossDomain: true,
+      dataType: "jsonp",
+      success: function (response) {
+        var place = 0
+        while (place < 12 ){
+          var linkListing = $('<li class="linkListing"></li>')
+          var text = response.results.collection1[place]["Link-Titles"].text;
+          var a = $('<a class="link" href=' + response.results.collection1[place]["Link-Titles"].href + '>'  + text + '</a><br>')
+          linkListing.append(a);
+          ul.append(linkListing);
+          place++;
+        };
+        ul.prepend(list);
+        $('.scotch-content').html(ul);
+      },
+      error: function (xhr, status) {
+      }
+      });
+    });
+
+$("body").on("click", "li#javascript_tag", function(event){
+    event.preventDefault();
+    var ul = $('<ul></ul>');
+    var response_1 = $.ajax({
+      url:"https://www.kimonolabs.com/api/cedzysr2?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+      crossDomain: true,
+      dataType: "jsonp",
+      success: function (response) {
+        var place = 0
+        var resources = $('<p></p>')
+        while (place < 6){
+          var linkListing = $('<li class="linkListing"></li>')
+          var text = response.results['Lessons'][place]['Titles'].text;
+          var a = $('<a class="link" href=' + response.results['Lessons'][place]['Titles'].href + '>'  + text + '</a><br>')
+          linkListing.append(a);
+          ul.append(linkListing)
+          place++;
+        };
+      },
+      error: function (xhr, status) {
+        }
+      });
+
+    var a = $('<a></a>');
+    var response_2 = $.ajax({
+        url:"https://www.kimonolabs.com/api/ejdzdt98?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          var linkListing = $('<li class="linkListing"></li>')
+          var title = response.results['collection1'][0]['Tite'];
+          linkListing.append(a)
+          a.append(title + '<br>');
+          ul.append(a);
+        },
+        error: function (xhr, status) {
+        }
+      });
+
+      var response_3 = $.ajax({
+        url:"https://www.kimonolabs.com/api/38uhdscy?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          var place = 0;
+          while (place < 19){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results.collection1[place]["Title-Links"].text;
+            var a = $('<a class="link" href=' + response.results.collection1[place]["Title-Links"].href + '>'  + text + '</a><br>')
+            linkListing.append(a);
+            ul.append(linkListing);
+            place++;
+        };
+          ul.prepend(list);
+          $('.scotch-content').html(ul);
+        },
+        error: function (xhr, status) {
+        }
+      });
+  });
+
+$("body").on("click", "li#mongo", function(event){
+  event.preventDefault();
+  var ul =$('<ul></ul>')
+  var response_python = $.ajax({
+    url:"https://www.kimonolabs.com/api/a0oyohre?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+    crossDomain: true,
+    dataType: "jsonp",
+    success: function (response) {
+      var place = 0
+          while (place < 11){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results['collection1'][place]['Title-Links'].text;
+            var a = $('<a class="link" href=' + response.results['collection1'][place]['Title-Links'].href + " " + 'class="selection"> '  + text + '</a><br>')
+            linkListing.append(a)
+            ul.append(linkListing);
+            place++;
+          };
+          ul.prepend(list);
+          $('.scotch-content').html(ul);
+    },
+    error: function (xhr, status) {
+    }
+    });
+  });
+
+$("body").on("click", "li#python", function(event){
+  event.preventDefault();
+  var ul =$('<ul></ul>')
+  var response_python = $.ajax({
+    url:"https://www.kimonolabs.com/api/5z3f5btc?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+    crossDomain: true,
+    dataType: "jsonp",
+    success: function (response) {
+      var place = 0
+          var resources = $('<p></p>')
+          while (place < 24){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results['collection2'][place]['Link-Titles'].text;
+            var a = $('<a class="link" href=' + response.results['collection2'][place]['Link-Titles'].href + '>'  + text + '</a><br>')
+            linkListing.append(a)
+            ul.append(linkListing);
+            place++;
+          };
+          ul.prepend(list);
+          $('.scotch-content').html(ul);
+    },
+    error: function (xhr, status) {
+    }
+    });
+  });
+
+$("body").on("click", "li#cs_tag", function(event){
+    event.preventDefault();
+    var ul = $('<ul></ul>')
+    var response_CS = $.ajax({
+        url:"https://www.kimonolabs.com/api/cmwe1bd0?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          var place = 0
+          var resources = $('<p></p>')
+          while (place < 3){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results['collection2'][place]['Lesson-Links'].text;
+            var a = $('<a class="link" href=' + response.results['collection2'][place]['Lesson-Links'].href + '>'  + text + '</a><br>')
+            linkListing.append(a);
+            ul.append(linkListing);
+            place++;
+          };
+            ul.prepend(list);
+            $('.scotch-content').html(ul);
+        },
+        error: function (xhr, status) {
+        }
+      })
+  });
+
+$("body").on("click", "li#angular_tag", function(event){
+  var ul = $('<ul></ul>');
+      var response_SA1 = $.ajax({
+        url:"https://www.kimonolabs.com/api/38uhdscy?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          var place = 0
+          while (place < 19){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results.collection1[place]["Link-Titles"].text;
+            var a = $('<a class="link" href=' + response.results.collection1[place]["Link-Titles"].href + '>'  + text + '</a><br>')
+            linkListing.append(a)
+            ul.append(linkListing);
+            place++;
+          };
+        },
+        error: function (xhr, status) {
+          }
+      });
+
+      var response_SA2 = $.ajax({
+        url:"https://www.kimonolabs.com/api/cy0kghuq?apikey=KqO43CQBHYvoDMtlWkXYeXMAa2K0or2M",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (response) {
+          console.log(response)
+          var place = 0;
+          while (place < 18){
+            var linkListing = $('<li class="linkListing"></li>')
+            var text = response.results.collection1[place]["Link-Titles"].text;
+            var a = $('<a class="link" href=' + response.results.collection1[place]["Link-Titles"].href + '>'  + text + '</a><br>')
+            linkListing.append(a);
+            ul.append(linkListing);
+            place++;
+          };
+          ul.prepend(list);
+        $('.scotch-content').html(ul);
+
+        },
+        error: function (xhr, status) {
+
+        }
+        });
 });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/assets/javascripts/curriculum2.js
+++ app/assets/javascripts/curriculum2.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+      var list = $('<ul></ul>');
 
   $('#git').on("click", function(evt) {
     evt.preventDefault();
@@ -19,6 +20,7 @@ $(document).ready(function() {
         ul.append(linkListing);
         i++;
       }
+      ul.prepend(list);
       var myModal = new Modal({
             content: "" + $(ul).html(),
             maxWidth: 600,
@@ -47,6 +49,7 @@ $(document).ready(function() {
         ul.append(linkListing);
         i++;
       }
+      ul.prepend(list);
       var myModal = new Modal({
             content: "" + $(ul).html(),
             maxWidth: 600,
@@ -59,31 +62,32 @@ $(document).ready(function() {
   })
 
     $('#jquery').on("click", function(evt) {
-    evt.preventDefault();
-    $.ajax({
-      url: '/curriculums/jquery',
-      type: 'GET'
-    })
-    .done(function(response) {
-      var ul = $('<ul></ul>');
-      var i = 0
-      var resources = $('<p></p>')
-      while (i < 8){
-        var linkListing = $('<li class="linkListing"></li>')
-        var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
-        linkListing.append(course);
-        ul.append(linkListing);
-        i++;
-      }
-      var myModal = new Modal({
-            content: "" + $(ul).html(),
-            maxWidth: 600,
-          });
-        myModal.open();
-    })
-    .fail(function(response) {
-      console.log("error")
-    })
+      evt.preventDefault();
+      $.ajax({
+        url: '/curriculums/jquery',
+        type: 'GET'
+      })
+      .done(function(response) {
+        var ul = $('<ul></ul>');
+        var i = 0
+        var resources = $('<p></p>')
+        while (i < 8){
+          var linkListing = $('<li class="linkListing"></li>')
+          var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
+          linkListing.append(course);
+          ul.append(linkListing);
+          i++;
+        }
+        ul.prepend(list);
+        var myModal = new Modal({
+              content: "" + $(ul).html(),
+              maxWidth: 600,
+            });
+          myModal.open();
+      })
+      .fail(function(response) {
+        console.log("error")
+      })
   })
 
      $('#sql').on("click", function(evt) {
@@ -104,6 +108,7 @@ $(document).ready(function() {
         ul.append(linkListing);
         i++;
       }
+      ul.prepend(list);
       var myModal = new Modal({
             content: "" + $(ul).html(),
             maxWidth: 600,
@@ -133,6 +138,7 @@ $('#sublime').on("click", function(evt) {
         ul.append(linkListing);
         i++;
       }
+      ul.prepend(list);
       var myModal = new Modal({
             content: "" + $(ul).html(),
             maxWidth: 600,
@@ -143,6 +149,135 @@ $('#sublime').on("click", function(evt) {
       console.log("error")
     })
   })
+
+$("body").on("click", "li#git_tag", function(event){
+    evt.preventDefault();
+    console.log("im from ajax")
+    $.ajax({
+      url: '/curriculums/git',
+      type: 'GET'
+    })
+    .done(function(response) {
+      var ul = $('<ul></ul>');
+      var i = 0
+      var resources = $('<p></p>')
+
+      while (i < 6){
+        var linkListing = $('<li class="linkListing"></li>')
+        var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
+        linkListing.append(course);
+        ul.append(linkListing);
+        i++;
+      }
+      ul.prepend(list);
+      $('.scotch-content').html(ul);
+    })
+    .fail(function(response) {
+      console.log("error")
+    })
+});
+
+$("body").on("click", "li#ruby_tag", function(event){
+    evt.preventDefault();
+    $.ajax({
+      url: '/curriculums/ruby',
+      type: 'GET'
+    })
+    .done(function(response) {
+      var ul = $('<ul></ul>');
+      var i = 0
+      var resources = $('<p></p>')
+      while (i < 16){
+        var linkListing = $('<li class="linkListing"></li>')
+        var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
+        linkListing.append(course);
+        ul.append(linkListing);
+        i++;
+      }
+      ul.prepend(list);
+      $('.scotch-content').html(ul);
+    })
+    .fail(function(response) {
+      console.log("error")
+    })
+});
+
+$("body").on("click", "li#sql_tag", function(event){
+  evt.preventDefault();
+    console.log("im from ajax")
+    $.ajax({
+      url: '/curriculums/sql',
+      type: 'GET'
+    })
+    .done(function(response) {
+      var ul = $('<ul></ul>');
+      var i = 0
+      var resources = $('<p></p>')
+      while (i < 3){
+        var linkListing = $('<li class="linkListing"></li>')
+        var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
+        linkListing.append(course);
+        ul.append(linkListing);
+        i++;
+      }
+      ul.prepend(list);
+      $('.scotch-content').html(ul);
+    })
+    .fail(function(response) {
+      console.log("error")
+    })
+});
+
+$("body").on("click", "li#jquery_tag", function(event){
+      evt.preventDefault();
+      $.ajax({
+        url: '/curriculums/jquery',
+        type: 'GET'
+      })
+      .done(function(response) {
+        var ul = $('<ul></ul>');
+        var i = 0
+        var resources = $('<p></p>')
+        while (i < 8){
+          var linkListing = $('<li class="linkListing"></li>')
+          var course = $('<a class="link" href=' + response[i].url +'>' + response[i].title + '</a>')
+          linkListing.append(course);
+          ul.append(linkListing);
+          i++;
+        }
+        ul.prepend(list);
+        $('.scotch-content').html(ul);
+      })
+      .fail(function(response) {
+        console.log("error")
+      })
+});
+
+$("body").on("click", "li#sublime_tag", function(event){
+    evt.preventDefault();
+    $.ajax({
+      url: '/curriculums/subl',
+      type: 'GET'
+    })
+    .done(function(response) {
+      console.log(response)
+      var ul = $('<ul></ul>');
+      var i = 0
+      var resources = $('<p></p>')
+      while (i < 4){
+        var linkListing = $('<li class="linkListing"></li>')
+        var course = $('<a class= "link" href=' + response[i].url +'>' + response[i].title + '</a>')
+        linkListing.append(course);
+        ul.append(linkListing);
+        i++;
+      }
+      ul.prepend(list);
+    $('.scotch-content').html(ul);
+    })
+    .fail(function(response) {
+      console.log("error")
+    })
+});
 
 
 
