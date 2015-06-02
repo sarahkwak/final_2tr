c53345b27b0f0a1f335a26702d52b0f05ed7571d
Sarah Kwak <sarahkwak@hotmail.com>
2015-06-01 21:56:16 -0700

    address has been added to all user type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510225251) do
+ActiveRecord::Schema.define(version: 20150602044612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150510225251) do
     t.string   "first_name"
     t.string   "description"
     t.string   "availability"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
